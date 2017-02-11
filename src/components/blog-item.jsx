ce6278b086dd70dab94712db7659c63180bb68f6
Igor Kim <yorikim@gmail.com>
2017-02-11 16:43:08 +0400

    Move blog items to constants

--- src/components/blog-item.jsx
+++ src/components/blog-item.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Image from './image'
 import TextBox from './text-box'
+import MetaInfo from './meta-info'
 
 const BlogItem = (props) => (
   <div>
     <Image width="100px" height="100px" {...props.image} />
     <TextBox text={props.text} />
+    <MetaInfo {...props.meta} />
   </div>
 )
 
---
+++ src/constants/blogItems.js
@@ -0,0 +1,28 @@
+const items = [
+  {
+    text: 'Test1',
+    image: {
+      src: "http://static.jsbin.com/images/dave.min.svg",
+      alt: 'Test1'
+    },
+    meta: {
+      author: 'Vasya',
+      createdAt: new Date(2017, 1, 1),
+      updatedAt: new Date(2017, 1, 2)
+    }
+  },
+  {
+    text: 'Test2',
+    image: {
+      src: "http://static.jsbin.com/images/dave.min.svg",
+      alt: 'Test2'
+    },
+    meta: {
+      author: 'Vanya',
+      createdAt: new Date(2017, 1, 11),
+      updatedAt: new Date(2017, 1, 12)
+    }
+  }
+]
+
+export {items}
