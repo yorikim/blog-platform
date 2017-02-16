3365ba728910e7454aa816d03e943cbb04f02ac4
Igor Kim <yorikim@gmail.com>
2017-02-16 22:02:20 +0400

    Fetch posts from REST API

--- src/components/widgets/post.jsx
+++ src/components/widgets/post.jsx
@@ -1,13 +1,26 @@
 import React, {PropTypes} from 'react';
 import BlogItem from './blog-item';
-import {items} from 'constants/blogItems';
 import Immutable from 'immutable';
+import request from 'superagent';
+import {server} from 'constants/settings';
 
 class Post extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {item: Immutable.fromJS(items[props.params.id - 1])};
+    this.state = {item: Immutable.fromJS({})};
+  }
+
+  componentDidMount() {
+    this.fetchPost();
+  }
+
+  fetchPost() {
+    request.get(
+      `${server}/posts/${this.props.id}`,
+      {},
+      (err, resp) => this.setState({item: Immutable.Map(resp.body)})
+    );
   }
 
   handleLike() {
